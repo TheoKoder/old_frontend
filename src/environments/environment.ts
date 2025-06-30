cd8c50fa2489f90e2b66d4661735dbba83c6248d
TheoKodar <t.rakomane55@gmail.com>
2025-06-30 12:26:23 +0200

    revision

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production:true,
-    AdminServerUrl: 'http://localhost:8082/',
-    PreyServerUrl :'http://localhost:8085/',
-    LaravelGates :'http://localhost/sp-mw/',
-    PreyFrontend:'http://localhost:8081/',
+    AdminServerUrl: 'http://',
+    PreyServerUrl :'http://localhost:9000/',
+    LaravelGates :'http://localhost:8082/',
+    PreyFrontend:'http://localhost:9000/',
     Map: {
-          accessToken: 'pk.eyJ1IjoibmVvemEiLCJhIjoiY2xvZnkwOTRiMHh1YTJrcndmam82em42aSJ9.DAxTwxCFRRjQ_BZ7y4ODgw',
-          centerLat: -26.20588864284071,
-          centerLng:  28.04367165035994,
+          accessToken: 'enter key here',
+          //centerLat: -26.20588864284071,
+          //centerLng:  28.04367165035994,
     }
 };
--- upload.bat
+++
@@ -1,1 +0,0 @@
-scp -r -i C:\Users\SPillay\Azure\egovkey.pem dist\web\browser\* egov@20.80.233.233:\home\egov\dat\
